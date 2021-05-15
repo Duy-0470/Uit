8dfc4c15b206f1d8ce71719e5ebe5a97d03e1ceb
Duy Hao Nguyen <duyhao301202@gmail.com>
2021-05-15 10:43:58 +0700

    Complex fixed

--- OOP/Tuan6/SoPhuc/COMPLEX.h
+++ OOP/Tuan6/SoPhuc/COMPLEX.h
@@ -1,77 +1,23 @@
-#ifndef COMPLEX_H_INCLUDED
-#define COMPLEX_H_INCLUDED
-
+#pragma once
+#ifndef CPX
+#define CPX
 #include <iostream>
-
 using namespace std;
 
-class COMPLEX
+class Complex
 {
-    private:
-        double real, img;
-    public:
-        COMPLEX(double REAL = 0, double IMG = 0)
-        {
-            real = REAL;
-            img = IMG;
-        }
+private:
+	double real, img;
 
-        friend ostream & operator << (ostream &out, const COMPLEX &s)
-        {
-            out << "Ket qua: ";
-            if (s.img < 0)
-            {
-                out << s.real << " - " << s.img*-1 << "i.\n";
-            }
-            else
-            {
-                out << s.real << " + " << s.img << "i.\n";
-            }
-            return out;
-        }
-
-        friend istream & operator >> (istream &in, COMPLEX &s)
-        {
-            cout << "Phan thuc: ";
-            in >> s.real;
-            cout << "Phan ao: ";
-            in >> s.img;
-            return in;
-        }
-
-        bool check()
-        {
-            if (real == 0 && img == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
-        COMPLEX operator + (COMPLEX &tmp)
-        {
-            COMPLEX tong(this->real + tmp.real, this->img + tmp.img);
-            return tong;
-        }
-        COMPLEX operator - (COMPLEX &tmp)
-        {
-            COMPLEX hieu(this->real - tmp.real, this->img - tmp.img);
-            return hieu;
-        }
-        COMPLEX operator * (COMPLEX &tmp)
-        {
-            COMPLEX tich(this->real * tmp.real - this->img * tmp.img, this->real * tmp.real + this->img * tmp.img);
-            return tich;
-        }
-        COMPLEX operator / (COMPLEX &tmp)
-        {
-            COMPLEX thuong;
-            thuong.real = (this->real * tmp.real + this->img * tmp.img) / (tmp.real*tmp.real + tmp.img*tmp.img);
-            thuong.img = (tmp.real * this->img - this->real * tmp.img) / (tmp.real*tmp.real + tmp.img*tmp.img);
-            return thuong;
-        }
+public:
+	Complex();
+	Complex(double, double);
+	friend Complex operator+(Complex, Complex);
+	friend Complex operator-(Complex, Complex);
+	friend Complex operator*(Complex, Complex);
+	friend Complex operator/(Complex, Complex);
+	friend istream& operator>>(istream&, Complex&);
+	friend ostream& operator<<(ostream&, Complex);
 };
 
-#endif // COMPLEX_H_INCLUDED
+#endif
