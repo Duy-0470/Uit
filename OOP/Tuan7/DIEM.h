02a1d477a0a2aabc8c251e4cb0b75d3271b3ff32
Duy Hao Nguyen <duyhao301202@gmail.com>
2021-05-16 21:55:41 +0700

    Week7 T + R

---
+++ OOP/Tuan7/DIEM.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef PNT
+#define PNT
+#include <iostream>
+using namespace std;
+
+class DIEM
+{
+private:
+	double x, y;
+
+public:
+	DIEM();
+	double GetX();
+	double GetY();
+	void SetXY(double, double);
+	double Distance(DIEM);
+	void Move(double, double);
+	friend ostream& operator<<(ostream&, DIEM);
+	friend istream& operator>>(istream&, DIEM&);
+};
+
+#endif
