3ec8c2cc6f0ef8b293a7f0e0402ed0f858da6eb9
Duy-0470 <20520470@gm.uit.edu.vn>
2021-06-25 19:48:07 +0700

    final

--- OOP/TT/Header.h
+++ OOP/TT/Header.h
@@ -5,22 +5,23 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 class TT {
 private:
-	string tens, ma, tacgia, theloai;
+	string tens, ma, tacgia, theloai; 
 	int nam = 0, xh = 0;
 public:
 	void nhap() {
 		cout << "Nhap ten sach: ";
 		cin.ignore(); getline(cin, tens);
-		cout << "Nhap ten tac gia: ";
-		getline(cin, tacgia);
 		cout << "Nhap ma: ";
 		getline(cin, ma);
 		cout << "Nhap the loai: ";
 		getline(cin, theloai);
+		cout << "Nhap ten tac gia: ";
+		getline(cin, tacgia);
 		do {
 			cout << "Nhap nam xuat ban: "; cin >> nam;
 		} while ( nam < 0);
@@ -52,7 +53,7 @@ public:
 	string getma() {
 		return ma;
 	}
-
+	
 	string getTl() {
 		return theloai;
 	}
@@ -93,20 +94,22 @@ public:
 class File {
 private:
 	vector<TT> a;
-	int sl;
+	int sl, count = 0;
 public:
 	void SetSl(int s){
 		sl = s;
 		a.resize(sl);
 	}
 
+	int GetSl() {
+		return sl;
+	}
+
 	void DocFile() {
-		fstream f("DocFile.txt", ios::in);
-		string t, m, tl, tg;
+		ifstream f("DocFile.txt", ios::in);
+		string t, m, tl, tg, space;
 		int n, xh;
-		f.open("DocFile.txt");
 		if (f.is_open()) {
-			f.ignore();
 			for (int i = 0; i < sl; i++) {
 				getline(f, t);
 				getline(f, m);
@@ -114,10 +117,14 @@ public:
 				getline(f, tg);
 				f >> n;
 				f >> xh;
-				
+				if (i != sl -1){
+					getline(f, space);
+				}
+				f.ignore();
 				a[i].SetS(t);
 				a[i].SetMa(m);
-				a[i].SetTG(tl);
+				a[i].SetTL(tl);
+				a[i].SetTG(tg);
 				a[i].SetNam(n);
 				a[i].SetXH(xh);
 			}
@@ -128,28 +135,139 @@ public:
 	}
 
 	void XuatVoFile() {
-		ofstream f("LuuFile.txt", ios::out);
-		string t, m, tl, tg;
-		int n, xh;
-
-		if (f.is_open()) {
-			for (int i = 0; i < sl; i++) {
-				f << "Ten sach: " << a[i].getS() << endl;
-				f << "Ma: " << a[i].getma() << endl;
-				f << "The loai: " << a[i].getTl() << endl;
-				f << "Tac gia: " << a[i].getTG() << endl;
-				f << "Nam xuat ban: " << a[i].getnam() << endl;
-				f << "Diem xep hang: " << a[i].getxh() << endl;
+			ofstream f("LuuFile.txt", ios::out);
+			string t, m, tl, tg;
+			int n, xh;
+			if (count != 0) {
+				ofstream ff("DocFile.txt", ios::out); 
+				for (int i = 0; i < sl; i++) { // getS, getTG, getTl, getnam, getxh, get 
+					ff << a[i].getS() << endl;
+					ff << a[i].getma() << endl;
+					ff << a[i].getTl() << endl;
+					ff << a[i].getTG() << endl;
+					ff << a[i].getnam() << endl;
+					ff << a[i].getxh() << endl;
+					if (i != sl - 1) {
+						ff << endl;
+						}
+				}
+				DocFile();
 			}
-		}
-		else cout << "Chua mo file" << endl;
-		f.close();
+			if (f.is_open()) {
+				for (int i = 0; i < sl; i++) {
+					f << "Ten sach: " << a[i].getS() << endl;
+					f << "Ma: " << a[i].getma() << endl;
+					f << "The loai: " << a[i].getTl() << endl;
+					f << "Tac gia: " << a[i].getTG() << endl;
+					f << "Nam xuat ban: " << a[i].getnam() << endl;
+					f << "Diem xep hang: " << a[i].getxh() << endl;
+					if (i != sl - 1) {
+						f << endl;
+					}
+				}
+			}
+			else cout << "Chua mo file" << endl;
+			f.close();
 	}
+	
 
 	void Nhap() {
-		sl = sl + 1;
-		a[sl].nhap();
+		SetSl(sl + 1);
+		a[sl-1].nhap();
+		count++;
 	}
+
+	string Thuong(string s)
+	{
+		for (int i = 0; i < s.length(); i++)
+		{
+			s[i] = tolower(s[i]);
+		}
+		return s;
+	}
+
+	void Tim() {
+		int dem = 0, aa;
+		string k;
+		cout << "1. Tim theo ten sach" << endl;
+		cout << "2. Tim theo ten tac gia" << endl;
+		cout << "3. Tim theo ma so" << endl;
+		cout << "Lua chon cua ban la: ";
+		cin >> aa;
+		cout << "-----------------" << endl;
+		switch (aa) {
+		case 1:
+			cout << "Nhap ten sach ban can tim: ";
+			cin.ignore();
+			getline(cin, k);
+			cout << "-----------------" << endl;
+			for (int i = 0; i < sl; i++) {
+				if (Thuong(a[i].getS()) == Thuong(k)) {
+					a[i].xuat();
+					dem++;
+				}
+
+			}
+			if (dem == 0) {
+				cout << "Khong co ten sach ma ban can tim " << endl;
+			}
+			return;
+		case 2:
+			cout << "Nhap ten tac gia ban can tim: ";
+			cin.ignore();
+			getline(cin, k);
+			cout << "-----------------" << endl;
+			for (int i = 0; i < sl; i++) {
+				if (Thuong(a[i].getTG()) == Thuong(k)) {
+					a[i].xuat();
+					dem++;
+				}
+
+			}
+			if (dem == 0) {
+				cout << "Khong co tac gia ma ban can tim " << endl;
+			}
+			return;
+		case 3:
+			cout << "Nhap ma so ban can tim: ";
+			cin.ignore();
+			getline(cin, k);
+			cout << "-----------------" << endl;
+			for (int i = 0; i < sl; i++) {
+				if (Thuong(a[i].getma()) == Thuong(k)) {
+					a[i].xuat();
+					dem++;
+				}
+
+			}
+			if (dem == 0) {
+				cout << "Khong co ma so ma ban can tim " << endl;
+			}
+			return;
+		}
+
+	}
+
+	void Xoa() {
+		string ms;
+		int sll = GetSl() -1;
+		int demm = 0;
+		cout << "Nhap ma so muon xoa: ";
+		cin.ignore();
+		getline(cin, ms);
+		for (int i = 0; i <= sll; i++) {
+			if (Thuong(a[i].getma()) == Thuong(ms))
+			{
+				a.erase(a.begin() + i);
+				demm++;
+				SetSl(sl - 1);
+				break;
+			}
+		}
+		if (demm == 0) cout << "Khong co ma so ma ban muon xoa" << endl;
+		
+	}
+	
 };
 
 #endif
