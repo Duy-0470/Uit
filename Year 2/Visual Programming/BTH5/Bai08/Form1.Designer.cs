1b048c94938746eb22636e94421c58b79d4522e2
12Dragon21 <80904153+12Dragon21@users.noreply.github.com>
2021-12-05 13:33:42 +0700

    Bai08 chua co double buffer

--- Year 2/Visual Programming/BTH5/Bai08/Form1.Designer.cs
+++ Year 2/Visual Programming/BTH5/Bai08/Form1.Designer.cs
@@ -29,24 +29,51 @@ namespace Bai08
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(436, 401);
+            this.ClientSize = new System.Drawing.Size(582, 503);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(600, 550);
+            this.MinimumSize = new System.Drawing.Size(600, 550);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Analog Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Year 2/Visual Programming/BTH5/Bai08/Form1.cs
+++ Year 2/Visual Programming/BTH5/Bai08/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,8 +17,14 @@ namespace Bai08
         public Form1()
         {
             InitializeComponent();
+            label1.Text = DateTime.Now.ToString();
             g = CreateGraphics();
             g.TranslateTransform(Size.Width / 2, Size.Height / 2);
+            DrawClock(g);
+            DrawHour();
+            DrawMin();
+            DrawSec();
+            timer1.Start();
         }
 
         private void DrawClock(Graphics g)
@@ -37,19 +44,87 @@ namespace Bai08
         }
 
         private void RotatePoint(Point[] pt, int iRotate, int iAngle)
-        {   
+        {
             Point temp = new Point(0, 0);
             for (int i = 0; i < iRotate; i++)
             {
                 temp.X = (int)(pt[i].X * Math.Cos(2 * Math.PI * iAngle / 360) - pt[i].Y * Math.Sin(2 * Math.PI * iAngle / 360));
                 temp.Y = (int)(pt[i].Y * Math.Cos(2 * Math.PI * iAngle / 360) + pt[i].X * Math.Sin(2 * Math.PI * iAngle / 360));
                 pt[i] = temp;
-            }    
+            }
+        }
+
+        private void DrawSec()
+        {
+            int sec = DateTime.Now.Second;
+            int degree = 180 - (sec * 6);
+            Point temp = new Point();
+            temp.X = (int)(130 * Math.Sin(2 * Math.PI * degree / 360));
+            temp.Y = (int)(130 * Math.Cos(2 * Math.PI * degree / 360));
+            g.DrawLine(new Pen(Color.White), new Point(0, 0), temp);
+        }
+        private void DrawMin()
+        {
+            int min = DateTime.Now.Minute;
+            int degree = 180 - (min * 6);
+            Point p1 = new Point();
+            p1.X = -(int)(15 * Math.Sin(2 * Math.PI * degree / 360));
+            p1.Y = -(int)(15 * Math.Cos(2 * Math.PI * degree / 360));
+            Point p2 = new Point();
+            p2.X = -(int)(15 * Math.Sin(2 * Math.PI * (degree + 90) / 360));
+            p2.Y = -(int)(15 * Math.Cos(2 * Math.PI * (degree + 90) / 360));
+            Point p3 = new Point();
+            p3.X = -(int)(15 * Math.Sin(2 * Math.PI * (degree - 90) / 360));
+            p3.Y = -(int)(15 * Math.Cos(2 * Math.PI * (degree - 90) / 360));
+            Point p4 = new Point();
+            p4.X = (int)(120 * Math.Sin(2 * Math.PI * degree / 360));
+            p4.Y = (int)(120 * Math.Cos(2 * Math.PI * degree / 360));
+            g.DrawLine(new Pen(Color.White), p1, p2);
+            g.DrawLine(new Pen(Color.White), p1, p3);
+            g.DrawLine(new Pen(Color.White), p4, p2);
+            g.DrawLine(new Pen(Color.White), p4, p3);
+        }
+        private void DrawHour()
+        {
+            int hour = DateTime.Now.Hour;
+            int min = DateTime.Now.Minute;
+            int degree = 180 - (hour * 30) - (int)(1.0 * min * 30 / 60);
+            Point p1 = new Point();
+            p1.X = -(int)(15 * Math.Sin(2 * Math.PI * degree / 360));
+            p1.Y = -(int)(15 * Math.Cos(2 * Math.PI * degree / 360));
+            Point p2 = new Point();
+            p2.X = -(int)(15 * Math.Sin(2 * Math.PI * (degree + 90) / 360));
+            p2.Y = -(int)(15 * Math.Cos(2 * Math.PI * (degree + 90) / 360));
+            Point p3 = new Point();
+            p3.X = -(int)(15 * Math.Sin(2 * Math.PI * (degree - 90) / 360));
+            p3.Y = -(int)(15 * Math.Cos(2 * Math.PI * (degree - 90) / 360));
+            Point p4 = new Point();
+            p4.X = (int)(90 * Math.Sin(2 * Math.PI * degree / 360));
+            p4.Y = (int)(90 * Math.Cos(2 * Math.PI * degree / 360));
+            g.DrawLine(new Pen(Color.White), p1, p2);
+            g.DrawLine(new Pen(Color.White), p1, p3);
+            g.DrawLine(new Pen(Color.White), p4, p2);
+            g.DrawLine(new Pen(Color.White), p4, p3);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             DrawClock(g);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+            label1.Text = DateTime.Now.ToString();
+            DrawClock(g);
+            DrawHour();
+            DrawMin();
+            DrawSec();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
